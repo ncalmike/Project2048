3b6171f02eeca268b2eef6f75d5147c6656f2cda
Mike Morse <mikemorse@appsnetconsulting.com>
2015-10-30 09:32:44 -0700

    combineAndMove functions rewritten into move functions and combine functions to prevent compound tile merging in single player move.
    Replaced initValue property with access function that returns variable
    initial value.
    
    Created score variable with access and mutate methods.
    
    Modified combineTiles to call setScore method and pass new tile total.
    
    Segregated like methods with formated remark statements.
    
    Removed System.out.print* statements that were used for testing.

--- src/BoardManager.java
+++ src/BoardManager.java
@@ -5,10 +5,11 @@ public class BoardManager {
 	// VARIABLE DECLERATION
 	private final	int 			NUM_ROWS	= 4;
 	private final 	int 			NUM_COLUMNS	= 4;
+	private 		int				score		= 0;
 	private 		GameTile[][] 	BoardTiles	= new GameTile[NUM_ROWS][NUM_COLUMNS];
-	private			int				initVal		= 2;
-	
-	// ALIGN FUNCTIONS MOVE VALUES TO TILES CLOSE TO SIDE THAT GAME PLAYER HAS SELECTED
+	/************************************************************************************/
+	// BoardManager INITIALIZATION FUNCTIONS
+	/************************************************************************************/	
 	BoardManager(){
 		initBoardTiles();
 		boolean trash = addTile(); // OUTPUT FROM addTile IS IRRELEVENT BECAUSE BOARD IS EMPTY
@@ -22,37 +23,25 @@ public class BoardManager {
 			}
 		}
 	}
-	
-	// ACTIVATE NEW GAME PIECE
-	private boolean addTile(){
-		// CREATING COORD CLASS TO STORE row AND col VALUES FOR TILES THAT HAVE A VALUE OF 0
-		class aryCoord{
-			int row;
-			int col;
-		}
-		
-		int numCoords 			= 0;
-		int coordIndex			= 0;
-		aryCoord[] emptyCells	= new aryCoord[16];
-		
-		for(int i = 0; i < NUM_ROWS; i++){
-			for(int j = 0; j < NUM_COLUMNS; j++){
-				if(BoardTiles[i][j].getValue() == 0){ // ADD REFERENCE TO TILES THAT CAN BE INITILIZED TO emptyCells ARRAY 
-					emptyCells[numCoords] = new aryCoord();
-					emptyCells[numCoords].row = i;
-					emptyCells[numCoords].col = j;
-					numCoords++;
-				}
-			}
-		}
-		if(numCoords > 0){
-			SecureRandom getRand = new SecureRandom();
-			coordIndex = getRand.nextInt(numCoords);
-			BoardTiles[emptyCells[coordIndex].row][emptyCells[coordIndex].col].setValue(this.initVal);
-		}
-		return numCoords > 0;
+	/************************************************************************************/
+	// GETTER METHODS TO RETURN INITIAL TILE VALUE OR CURRENT SCORE
+	/************************************************************************************/
+	public int getInitVal(){
+		SecureRandom getRand = new SecureRandom();
+		return (getRand.nextInt(2) + 1) * 2;
 	}
-	
+	public int getScore(){
+		return this.score;
+	}
+	/************************************************************************************/
+	// SETTER METHOD TO INCREMENT SCORE BY VALUE OF COMBINED TILES
+	/************************************************************************************/
+	public void setScore(int toAdd){
+		this.score += toAdd;
+	}
+	/************************************************************************************/
+	// FUNCTIONS TO TEST IF TILE CAN BE ADDED AND TO ADD A TILE TO THE GAMEBOARD
+	/************************************************************************************/
 	// TEST THAT BoardTiles HAS AT LEAST ONE TILE THAT CAN ACCEPT INITIALIZED VALUE
 	private boolean canAddTile(){
 		boolean canAdd 	= false;
@@ -72,132 +61,227 @@ public class BoardManager {
 		
 		return canAdd;
 	}
-	
-	// LOOP THROUGH ROWS OR COLUMNS AND CALL CORRECT FUNCTION TO ALIGN AND MERGE TILES
-	public void alignNorth(boolean addNewTile){
-		for(int k = 0; k < NUM_ROWS; k++){
-			combineAndMoveNorth(k);
+	// ACTIVATE NEW GAME PIECE
+	private boolean addTile(){
+		// CREATING COORD CLASS TO STORE row AND col VALUES FOR TILES THAT HAVE A VALUE OF 0
+		class aryCoord{
+			int row;
+			int col;
 		}
-		isBoardFull();
-	}
-	public void alignSouth(boolean addNewTile){
-		for(int k = 0; k < NUM_ROWS; k++){
-			combineAndMoveSouth(k);
-		}
-		isBoardFull();
-	}
-	public void alignEast(boolean addNewTile){
-		for(int k = 0; k < NUM_ROWS; k++){
-			combineAndMoveEast(k);
-		}
-		isBoardFull();
-	}
-	public void alignWest(boolean addNewTile){
-		for(int k = 0; k < NUM_ROWS; k++){
-			combineAndMoveWest(k);
-		}
-		isBoardFull();
-	}
-
-	// JOIN LIKE VALUES, ALIGN TILES, AND ADD A NEW TILE IF addNewTile IS TRUE
-	public boolean combineAndMoveNorth(int index){
-		boolean combinedTiles = false;
-		for(int i = 1; i < NUM_ROWS; i++){
-			
-			if(BoardTiles[i][index].getValue() > 0){
-				// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
-				while(i > 0 && canMove(BoardTiles[i][index], BoardTiles[i-1][index])){
-					moveTileValues(BoardTiles[i][index], BoardTiles[i-1][index]);
-					i--;
-				}
-				while(i > 0 && canCombine(BoardTiles[i][index], BoardTiles[i-1][index])){
-					combinedTiles = true;
-					marryTileValues(BoardTiles[i][index], BoardTiles[i-1][index]);
-					i--;
+		
+		int numCoords 			= 0;
+		int coordIndex			= 0;
+		aryCoord[] emptyCells	= new aryCoord[NUM_ROWS * NUM_COLUMNS];
+		
+		for(int i = 0; i < NUM_ROWS; i++){
+			for(int j = 0; j < NUM_COLUMNS; j++){
+				if(BoardTiles[i][j].getValue() == 0){ // ADD REFERENCE TO TILES THAT CAN BE INITILIZED TO emptyCells ARRAY 
+					emptyCells[numCoords] = new aryCoord();
+					emptyCells[numCoords].row = i;
+					emptyCells[numCoords].col = j;
+					numCoords++;
 				}
 			}
-			
 		}
-		return combinedTiles;
-	}
-	public boolean combineAndMoveSouth(int index){
-		boolean combinedTiles = false;
-		for(int i = NUM_ROWS - 1; i >= 0 ; i--){
-			if(BoardTiles[i][index].getValue() > 0){
-				// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
-				while(i < NUM_ROWS - 1 && canMove(BoardTiles[i][index], BoardTiles[i+1][index])){
-					moveTileValues(BoardTiles[i][index], BoardTiles[i+1][index]);
-					i++;
-				}
-				while(i < NUM_ROWS - 1 && canCombine(BoardTiles[i][index], BoardTiles[i+1][index])){
-					combinedTiles = true;
-					marryTileValues(BoardTiles[i][index], BoardTiles[i+1][index]);
-					i++;
-				}
-			}
-			
+		if(numCoords > 0){
+			SecureRandom getRand = new SecureRandom();
+			coordIndex = getRand.nextInt(numCoords);
+			BoardTiles[emptyCells[coordIndex].row][emptyCells[coordIndex].col].setValue(this.getInitVal());
 		}
-		return combinedTiles;
+		return numCoords > 0;
 	}
-	public boolean combineAndMoveEast(int index){
-		boolean combinedTiles = false;
-		for(int i = NUM_ROWS - 1; i >= 0 ; i--){
-			if(BoardTiles[index][i].getValue() > 0){
-				// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
-				while(i < NUM_ROWS - 1 && canMove(BoardTiles[index][i], BoardTiles[index][i+1])){
-					moveTileValues(BoardTiles[index][i], BoardTiles[index][i+1]);
-					i++;
-				}
-				while(i < NUM_ROWS - 1 && canCombine(BoardTiles[index][i], BoardTiles[index][i+1])){
-					combinedTiles = true;
-					marryTileValues(BoardTiles[index][i], BoardTiles[index][i+1]);
-					i++;
-				}
-			}
-			
-		}
-		return combinedTiles;
+	/************************************************************************************/
+	// TEST CAN SWAP, TEST CAN MERGE, SWAP, AND MERGE TILES METHODS
+	/************************************************************************************/
+	public boolean tileHasValue(GameTile tile){
+		return tile.getValue() > 0;
 	}
-	
-	public boolean combineAndMoveWest(int index){
-		boolean combinedTiles = false;
-		for(int i = 1; i < NUM_ROWS; i++){
-			
-			if(BoardTiles[index][i].getValue() > 0){
-				// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
-				while(i > 0 && canMove(BoardTiles[index][i], BoardTiles[index][i-1])){
-					moveTileValues(BoardTiles[index][i], BoardTiles[index][i-1]);
-					i--;
-				}
-				while(i > 0 && canCombine(BoardTiles[index][i], BoardTiles[index][i-1])){
-					combinedTiles = true;
-					marryTileValues(BoardTiles[index][i], BoardTiles[index][i-1]);
-					i--;
-				}
-			}
-			
-		}
-		return combinedTiles;
-	}
-	
-	// TEST, SWAP, AND MERGE METHODS
 	public boolean canMove(GameTile tile1, GameTile tile2){
 		return tile1.getValue() > 0 && tile2.getValue() == 0;
 	}
 	public boolean canCombine(GameTile tile1, GameTile tile2){
 		return tile1.getValue() == tile2.getValue();
 	}
-	public void moveTileValues(GameTile tile1, GameTile tile2){
+	public boolean moveTileValues(GameTile tile1, GameTile tile2){
 		tile2.setValue(tile1.getValue());
 		tile1.setValue(0);
+		return true;
 	}
-	public void marryTileValues(GameTile tile1, GameTile tile2){
-		System.out.println("Marrying values");
-		tile2.setValue(tile1.getValue() + tile2.getValue());
+	public boolean marryTileValues(GameTile tile1, GameTile tile2){
+		int combinedValue = tile1.getValue() + tile2.getValue();
+		tile2.setValue(combinedValue);
 		tile1.setValue(0);
+		this.setScore(combinedValue);
+		return true;
 	}
-	
+	/************************************************************************************/
+	// JOIN LIKE VALUES, ALIGN TILES, AND ADD A NEW TILE IF addNewTile IS TRUE
+	/************************************************************************************/
+	// REMARKS DESCRIBE ALL ALIGN FUNCTIONS
+	public void alignNorth(boolean addNewTile){
+		boolean qualified			= false;
+		// LOOP THROUGH ROWS OR COLUMNS AND CALL SPECIFIC MOVE AND ALIGN FUNCTIONS
+		for(int k = 0; k < NUM_ROWS; k++){
+			boolean firstMove 		= moveNorth(k);
+			boolean combined		= combineNorth(k);
+			moveNorth(k);
+			// ASSIGN qualified TO TRUE IF TILE HAS BEEN MOVED OR COMBINED ON THIS OR PREVIOUS ITERATIONS
+			qualified 				= qualified || firstMove || combined;
+		}
+		// CALL isBoardFull TO SEE IF OUT OF MOVES AND PASS true IF TILE WAS MOVED OR COMBINED
+		isBoardFull(qualified);
+	}
+	public void alignSouth(boolean addNewTile){
+		boolean qualified			= false;
+		for(int k = 0; k < NUM_ROWS; k++){
+			boolean firstMove 		= moveSouth(k);
+			boolean combined		= combineSouth(k);
+			moveSouth(k);
+			qualified 				= qualified || firstMove || combined;
+		}
+		isBoardFull(qualified);
+	}
+	public void alignEast(boolean addNewTile){
+		boolean qualified			= false;
+		for(int k = 0; k < NUM_ROWS; k++){
+			boolean firstMove 		= moveEast(k);
+			boolean combined		= combineWest(k);
+			moveEast(k);
+			qualified 				= qualified || firstMove || combined;
+		}
+		isBoardFull(qualified);
+	}
+	public void alignWest(boolean addNewTile){
+		boolean qualified			= false;
+		for(int k = 0; k < NUM_ROWS; k++){
+			boolean firstMove 		= moveWest(k);
+			boolean combined		= combineWest(k);
+			moveWest(k);
+			qualified 				= qualified || firstMove || combined;
+		}
+		isBoardFull(qualified);
+	}
+	/************************************************************************************/
+	// MOVE VALUES TO TILES CLOSE TO SIDE THAT GAME PLAYER HAS SELECTED
+	/************************************************************************************/
+	// REMARKS DESCRIBE ALL MOVE FUNCTIONS
+	public boolean moveNorth(int index){
+		boolean movedTiles = false;
+		// LOOP THROUGH ROWS OR COLUMNS
+		for(int i = 1; i < NUM_ROWS; i++){
+			// TEST THAT TILE IN CURRENT ROW AND COLUMN IS AN ACTIVE GAME PIECE (value > 0)
+			if(BoardTiles[i][index].getValue() > 0){
+				// CONTINUE TO MOVE TILE VALUE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
+				while(i > 0 && canMove(BoardTiles[i][index], BoardTiles[i-1][index])){
+					movedTiles = moveTileValues(BoardTiles[i][index], BoardTiles[i-1][index]);
+					// INCREMENT i IN DIRECTION OF PLAYER MOVE
+					i--;
+				}
+			}
+		}
+		return movedTiles;
+	}
+	public boolean moveSouth(int index){
+		boolean movedTiles = false;
+		for(int i = NUM_COLUMNS - 1; i >= 0 ; i--){
+			if(BoardTiles[i][index].getValue() > 0){
+				while(i < NUM_COLUMNS - 1 && canMove(BoardTiles[i][index], BoardTiles[i+1][index])){
+					movedTiles = moveTileValues(BoardTiles[i][index], BoardTiles[i+1][index]);
+					i++;
+				}
+			}
+			
+		}
+		return movedTiles;
+	}
+	public boolean moveEast(int index){
+		boolean movedTiles = false;
+		for(int i = NUM_ROWS - 1; i >= 0 ; i--){
+			if(BoardTiles[index][i].getValue() > 0){
+				while(i < NUM_ROWS - 1 && canMove(BoardTiles[index][i], BoardTiles[index][i+1])){
+					movedTiles = moveTileValues(BoardTiles[index][i], BoardTiles[index][i+1]);
+					i++;
+				}
+			}
+			
+		}
+		return movedTiles;
+	}
+	public boolean moveWest(int index){
+		boolean movedTiles = false;
+		for(int i = 1; i < NUM_ROWS; i++){
+			
+			if(BoardTiles[index][i].getValue() > 0){
+				while(i > 0 && canMove(BoardTiles[index][i], BoardTiles[index][i-1])){
+					movedTiles = moveTileValues(BoardTiles[index][i], BoardTiles[index][i-1]);
+					i--;
+				}
+			}
+			// IF CHANGE ZERO IN LEFT ROW, DO HERE
+			
+		}
+		return movedTiles;
+	}
+	/************************************************************************************/
+	// COMBILE TILES WITH SAME VALUE WHEN NEXT TO EACH OTHER IN DIRECTON OF PLAYER MOVE 
+	/************************************************************************************/
+	public boolean combineNorth(int index){
+		boolean combinedTiles = false;
+		for(int i = 1; i < NUM_COLUMNS; i++){
+			// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
+			if(tileHasValue(BoardTiles[i][index]) && canCombine(BoardTiles[i][index], BoardTiles[i-1][index])){
+				combinedTiles = marryTileValues(BoardTiles[i-1][index], BoardTiles[i][index]);
+				i++;
+			}
+		}
+		return combinedTiles;
+	}public boolean combineSouth(int index){
+		boolean combinedTiles = false;
+		for(int i = NUM_COLUMNS - 2; i > 0 ; i--){
+			// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
+			if(tileHasValue(BoardTiles[i][index]) && canCombine(BoardTiles[i][index], BoardTiles[i+1][index])){
+				combinedTiles = marryTileValues(BoardTiles[i+1][index], BoardTiles[i][index]);
+				i++;
+			}
+		}
+		return combinedTiles;
+	}
+	public boolean combineEast(int index){
+		boolean combinedTiles = false;
+		for(int i = NUM_COLUMNS - 2; i > 0 ; i--){
+			// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
+			if(tileHasValue(BoardTiles[index][i]) && canCombine(BoardTiles[index][i], BoardTiles[index][i+1])){
+				combinedTiles = marryTileValues(BoardTiles[index][i+1], BoardTiles[index][i]);
+				i++;
+			}
+		}
+		return combinedTiles;
+	}
+	public boolean combineWest(int index){
+		boolean combinedTiles = false;
+		for(int i = 1; i < NUM_ROWS; i++){
+			// CONTINUE TO MOVE TILE UNTIL BLOCKED BY TILE THAT HAS VALUE > 0
+			if(tileHasValue(BoardTiles[index][i]) && canCombine(BoardTiles[index][i], BoardTiles[index][i-1])){
+				combinedTiles = marryTileValues(BoardTiles[index][i-1], BoardTiles[index][i]);
+				i++;
+			}
+		}
+		return combinedTiles;
+	}
+	/************************************************************************************/
+	// TEST IF ALL TILES HAVE A VALUE GREATER THAN ZERO
+	/************************************************************************************/
+	public void isBoardFull(boolean moveMade){
+		if(moveMade && canAddTile()){
+			addTile();
+		} else {
+			if(moveMade)
+				System.out.println("No more moves can be made!");
+		}
+	}
+	/************************************************************************************/
 	// toString METHOD TO RETURN TILE VALUES IN TABBED OUTPUT FOR TESTING PURPOSES
+	/************************************************************************************/
 	@Override
 	public String toString() {
 		String toRtrn = "";
@@ -207,16 +291,12 @@ public class BoardManager {
 			}
 			toRtrn += "\n";
 		}
+		toRtrn += "\nScore: " + this.getScore() + "\n\n";
 		return toRtrn;
 	}
-	
-	// TEST IF ALL TILES HAVE A VALUE GREATER THAN ZERO
-	public void isBoardFull(){
-		if(canAddTile()){
-			addTile();
-		} else {
-			System.out.println("No more moves can be made!");
-		}
-	}
+	/************************************************************************************/
+	// END OF BoarManager class
+	/************************************************************************************/
 }
+
 
